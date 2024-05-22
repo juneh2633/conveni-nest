c95ba27342b77989df7eb3a69a7822e3f6f80d3d
juneh2633 <juneh2633@gmail.com>
2024-05-22 15:41:22 +0900

    chore: change model name

---
+++ src/modules/auth/model/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+  idx: number;
+  createdAt: Date;
+  email: string;
+  nickname: string;
+  rankIdx: number;
+}
--- src/modules/auth/token.service.ts
+++ src/modules/auth/token.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
-import { Payload } from './model/payload.model';
+import { User } from './model/user.model';
 
 @Injectable()
 export class TokenService {
   constructor(private readonly jwtService: JwtService) {}
 
-  verifyToken(bearerToken: string): Payload {
+  verifyToken(bearerToken: string): User {
     try {
       const token = bearerToken.split(' ')[1];
-      const payload = this.jwtService.verify<Payload>(token);
+      const payload = this.jwtService.verify<User>(token);
 
       return payload;
     } catch (err) {
@@ -27,7 +27,7 @@ export class TokenService {
     }
   }
 
-  noLoginToken(): Payload {
+  noLoginToken(): User {
     return {
       idx: 1,
       createdAt: null,
