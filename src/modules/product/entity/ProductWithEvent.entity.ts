6520feccaa5d1c83e601ff646613a4a0e57051eb
juneh2633 <juneh2633@gmail.com>
2024-05-30 00:08:43 +0900

    refactor: product with Event Entity

---
+++ src/modules/product/entity/ProductWithEvent.entity.ts
@@ -0,0 +1,47 @@
+import { EventHistory } from '@prisma/client';
+import { ProductWithBookmark } from '../model/product-with-bookmark.model';
+
+export class ProductWithEventEntity {
+  idx: number;
+  categoryIdx: number;
+  name: string;
+  price: string;
+  productImg: string;
+  bookmarked: boolean;
+  score: string;
+  createdAt: Date;
+  events: any;
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  public static create(
+    productList: ProductWithBookmark[],
+    eventList: EventHistory[],
+  ): ProductWithEventEntity[] {
+    const eventMap = eventList.reduce((acc, event) => {
+      if (!acc[event?.productIdx]) {
+        acc[event?.productIdx] = [];
+      }
+      acc[event.productIdx].push({
+        companyIdx: event?.companyIdx,
+        eventIdx: event?.eventIdx,
+        price: event?.price,
+      });
+      return acc;
+    }, {});
+
+    return productList.map((product) => {
+      return new ProductWithEventEntity({
+        idx: product.idx,
+        categoryIdx: product.categoryIdx,
+        name: product.name,
+        price: product.price,
+        productImg: product.productImg,
+        bookmarked: product.bookmarked,
+        createdAt: product.createdAt,
+        events: eventMap[product.idx] || [],
+      });
+    });
+  }
+}
