3247427046bd8e3c5c1ee08f4aafcf1e01ce03ef
juneh2633 <juneh2633@gmail.com>
2024-06-01 11:26:04 +0900

    fix: minimum page number

--- src/modules/product/dto/request/get-products-pageble.dto.ts
+++ src/modules/product/dto/request/get-products-pageble.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 
-import { IsNumber } from 'class-validator';
+import { IsNumber, Min } from 'class-validator';
 
 export class GetProductsPagebleDto {
+  /**
+   * 페이지 1이상 정수
+   * @example 1
+   */
   @IsNumber()
   @Type(() => Number)
+  @Min(1, { message: 'Page number must be at least 1' })
   page: number;
 }
